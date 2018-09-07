e0bea54588e2a9bdc885f836c6b5367f4fcb4e0d
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-07 16:07:08 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+
+#turn_count
+board = ["","","","","","","", "", ""]
+def turn_count
+  board.each do |box|
+end
+
+#current_player
+def current_player
+if board % 2 == 0 
+  puts "O"
+else
+  puts "X"
+end
+end
+end
